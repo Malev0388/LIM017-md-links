f3a08702f2a78037b6c7d7c6b3e15c39764b5fb5
malev.dg <malev.dg@outlook.com>
2022-06-01 20:27:18 -0500

    leyendo archivos y directorios con fs

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-module.exports = () => {
+/*module.exports = () => {
   // ...
-};
+};*/
+
+const fs = require('node:fs');
+
+/*fs.writeFile('./texto.txt', 'prueba 1', function(error){
+  if(error){
+    console.log(error);
+  }
+});*/
+
+/*fs.readFile('./texto.txt', (error, data)=>{
+  if(error){
+    console.log(error)
+  }
+  console.log(data.toString());
+})*/
+
+fs.readdir('./', (error, archivos)=>{
+  archivos.forEach(archivos =>{
+    console.log(archivos);
+  });
+});
